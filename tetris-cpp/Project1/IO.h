1b41c30dbd08117d8426a1c06fba737409a49c6c
tslothorst <t.slothorst@gmail.com>
2021-03-14 20:40:36 +0100

    xxx

--- tetris-cpp/Project1/IO.h
+++ tetris-cpp/Project1/IO.h
@@ -3,10 +3,10 @@
 #define SDL_MAIN_HANDLED
 #ifndef LINUX
 #include "SDL/include/SDL.h"							
-#include "SDL/SDL_GfxPrimitives/SDL_gfxPrimitives.h"
+#include "sdl_gfxPrimitives.h"
 #else
 #include <SDL/SDL.h>									
-#include "SDL/SDL_GfxPrimitives/sdl_gfxprimitives.h"	
+#include "sdl_gfxprimitives.h"	
 #endif
 #pragma comment (lib, "SDL.lib")
 #pragma comment (lib, "SDL_GfxPrimitives_Static.lib")
