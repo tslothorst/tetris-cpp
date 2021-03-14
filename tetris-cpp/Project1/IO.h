6b43f3a7395e85678b64664d4f906646dcbb1c8c
tslothorst <t.slothorst@gmail.com>
2021-03-14 19:14:49 +0100

    Add SDL_MAIN_HANDLED

--- tetris-cpp/Project1/IO.h
+++ tetris-cpp/Project1/IO.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #ifndef LINUX
+#define SDL_MAIN_HANDLED
 #include "SDL/include/SDL.h"							
 #include "SDL/SDL_GfxPrimitives/SDL_gfxPrimitives.h"
 #else
+#define SDL_MAIN_HANDLED
 #include <SDL/SDL.h>									
 #include "SDL/SDL_GfxPrimitives/sdl_gfxprimitives.h"	
 #endif
