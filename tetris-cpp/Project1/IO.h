d8d23aa8d067c9ec45a0802a81d907aaeb2fb8c1
tslothorst <t.slothorst@gmail.com>
2021-03-14 20:24:23 +0100

    xxx

--- tetris-cpp/Project1/IO.h
+++ tetris-cpp/Project1/IO.h
@@ -8,8 +8,8 @@
 #include <SDL/SDL.h>									
 #include "SDL/SDL_GfxPrimitives/sdl_gfxprimitives.h"	
 #endif
-#pragma comment (lib, "../SDL/lib/x64/SDL.lib")
-#pragma comment (lib, "../SDL/SDL_GfxPrimitives/SDL_GfxPrimitives_Static.lib")
+#pragma comment (lib, "SDL\lib\x64\SDL.lib")
+#pragma comment (lib, "SDL\SDL_GfxPrimitives\SDL_GfxPrimitives_Static.lib")
 
 enum color { BLACK, RED, GREEN, BLUE, CYAN, MAGENTA, YELLOW, WHITE, COLOR_MAX }; // Colors
 
